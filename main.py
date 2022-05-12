ba6d6d09cc0b7d3d001171b8ac9b72e7e4ae430a
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-05-12 18:18:12 +0300

    add test

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ from numpy import random
 import numpy as np
 import array as arr
 
+
 class QAP:
     def __init__(self, distances,
                  flows):
         # self.A = np.matrix(assignments) # Матрица стоимости назначений
-        self.D = np.matrix(distances) # Матрица стоимости перевозки
-        self.F = np.matrix(flows) # Количество единиц ресурса
+        self.D = np.matrix(distances)  # Матрица стоимости перевозки
+        self.F = np.matrix(flows)  # Количество единиц ресурса
         self.size = len(self.D)
         # IWO параметры
         self.maxIt = 20  # Максимальное количество итераций
-        self.population_Size_Initial = 20  # Начальный размер популяции
-        self.maximum_Population_Size = 15  # Максимальная размер популяции
-        self.min_Seed = 4  # Минимальное количество семян
-        self.max_Seed = 8  # Максимальное количество семян
+        self.population_Size_Initial = 2000  # Начальный размер популяции
+        self.maximum_Population_Size = 20  # Максимальная размер популяции
+        self.min_Seed = 1  # Минимальное количество семян
+        self.max_Seed = 3  # Максимальное количество семян
 
         self.m = 3  # Показатель уменьшения дисперсии(m)
 
@@ -29,42 +30,42 @@ class QAP:
         cost = 0
         # for i in range(len(p)):  # i - объект, site - расположение
         #     cost += self.A[p[i], i]  # Находим сумма стоимости расположений объекта
-
         # f(i,j)*d(pi,pj)
         for i in range(1, len(p)):
             for j in range(i):
                 cost += self.F[i, j] * self.D[p[i], p[j]]
 
-        return cost*2
+        return cost * 2
 
     # Основной алгоритм
     def start(self):
 
-        population = list() # Начальная популяция
+        population = list()  # Начальная популяция
         # Генерируем начальную популяцию
         for i in range(self.population_Size_Initial):
-
             # Генерируем семя
-            seed = np.array([random.uniform(-10,10) for i in range(len(self.D))]) # Равномерное распределение
+            seed = np.array([random.uniform(-5, 5) for i in range(len(self.D))])  # Равномерное распределение
             rand = seed
-            seed = sorted(range(len(self.D)), key=lambda i: seed[i]) # Исходное семя
+            seed = sorted(range(len(self.D)), key=lambda i: seed[i])  # Исходное семя
 
             # Добавляем в популяцию, вычисляется целевая функция
-            population.append((self.target_function(seed),tuple(seed),tuple(rand)))
+            population.append((self.target_function(seed), tuple(seed), tuple(rand)))
 
-        population.sort(reverse=True) # Сортируем
+        population.sort(reverse=True)  # Сортируем
 
         # Вычисления ведутся пока не не достигнуто конечное число операций
         for t in range(0, self.maxIt):
             # Обновить стандартное отклонение по формуле
-            sigma = (pow(((self.maxIt - t) / self.maxIt), self.m) * (self.sigma_initial - self.sigma_final)) + self.sigma_final
-            best_Solution = min(population)[0] # Лучшее значение
-            worst_Solution = max(population)[0] # Худшее значение
+            sigma = (pow(((self.maxIt - t) / self.maxIt), self.m) * (
+                    self.sigma_initial - self.sigma_final)) + self.sigma_final
+            best_Solution = min(population)[0]  # Лучшее значение
+            worst_Solution = max(population)[0]  # Худшее значение
             # фаза воспроизводства
             for i in range(0, len(population)):
 
                 # Вычисляем число семян, которые может произвести данный сорняк
-                ratio = float((int(population[i][0]) - int(worst_Solution)) / (int(best_Solution) - int(worst_Solution) + 1))
+                ratio = float(
+                    (int(population[i][0]) - int(worst_Solution)) / (int(best_Solution) - int(worst_Solution) + 1))
                 s = (self.min_Seed + ((self.max_Seed - self.min_Seed) * ratio))
 
                 # Каждое семя
@@ -84,38 +85,33 @@ class QAP:
 
             # Исключаем слабых
             population = population[0:self.maximum_Population_Size]
-            #print(population[0:5])
+            # print(population[0:5])
             print("Итерация : " + str(t) + "    Лучшее " + str(population[0][0]) + str(population[0][1]))
 
     def stop(self):
-        print(self.target_function([6, 4, 11,  1,  0,  2,  8, 10, 9,  5,  7,  3]))
+        print(self.target_function([7, 15, 13, 16, 3, 10, 2, 18, 6, 8, 0, 14, 5, 12, 9, 1, 4, 19, 17, 11]))
+
 
 def main():
-    # qap = QAP([[9, 51, 3], [2, 4, 1], [6, 22, 7]],[[0, 70, 2], [7, 0, 43], [2, 41, 0]],[[0, 31, 6], [3, 0, 42], [6, 4, 0]])
-    # qap.start()
-    # n = 20
-    # matrix_A = [[random.randint(1, 100) for j in range(n)] for i in range(n)]
-    # matrix_B = [[random.randint(1, 100) for j in range(n)] for i in range(n)]
-    # matrix_C = [[random.randint(1, 100) for j in range(n)] for i in range(n)]
-    #
-    # for i in range(n):
-    #     for j in range(n):
-    #         if i == j:
-    #             matrix_B[i][j] = 0
-    #             matrix_C[i][j] = 0
-
+    # получим объект файла
+    file1 = open("tai256c.txt", "r")
     data_F = []
-    with open("chr12a(F).txt") as f:
-        for line in f:
-            data_F.append([int(x) for x in line.split()])
     data_D = []
-    with open("chr12a(D).txt") as f:
-        for line in f:
-            data_D.append([int(x) for x in line.split()])
+
+    # считываем строку
+    size = file1.readline()
+    file1.readline()
+    for i in range(int(size)):
+        data_F.append([int(x) for x in file1.readline().split()])
+    for i in range(int(size)):
+        data_D.append([int(x) for x in file1.readline().split()])
+    # print(data_D)
+    # print(data_F)
+    # закрываем файл
+    file1.close()
     qap = QAP(data_D, data_F)
     qap.start()
-    qap.stop()
-
+    #qap.stop()
 
 
 if __name__ == '__main__':
